35ca0920b1b92f3f8200e16905aafdabda10dce7
shane adams <adamsch1@yahoo.com>
2013-10-07 10:21:19 -0700

    test returning data

--- controllers/test.c
+++ controllers/test.c
@@ -1,21 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#include <event2/buffer.h>
-#include <event2/keyvalq_struct.h>
-
 #include "rub.h"
 
-f
+int k=0;
 int main( int argc, char **argv ) {
-  struct rub_t *rub = rub_get_request();
-  struct evkeyval *kv;
+  struct client_t *client = rub_get_request();
 
-  for( kv = rub->post_data->tqh_first; 
-       kv != NULL; 
-       kv = kv->next.tqe_next ) {
-    evbuffer_add_printf( rub->evb, "%s=%s<br>", kv->key, kv->value );
-  }
-  evbuffer_add_printf( rub->evb, "Dude!");
+  
+  bappend_printf( &client->outs, "<html>Count: %d</html>", ++k);  
   return 200;
 }
